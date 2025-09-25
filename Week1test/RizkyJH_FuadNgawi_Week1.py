275fb54575a6bd7cb6c6ed092bb06c1823ebd749
RizkyJH <firecalm2@gmail.com>
2025-09-25 09:59:29 +0700

    Create RizkyJH_FuadNgawi_Week1.py

---
+++ Week1test/RizkyJH_FuadNgawi_Week1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+data = {
+    "Name": ["RizkyJH"],
+    "Age": [21],
+    "Oshi JMK48": ["FuadNgawi"],
+    "Reason to be a barudakers": ["SolidSolidSolid"]
+}
+
+df = pd.DataFrame(data)
+print(df)
